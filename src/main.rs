28a1cb68143b662553e9438409c1df6a67d1de1c
Albert <noe.thalheim@gmail.com>
2017-02-10 18:34:30 +0100

    Add main.rs

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+extern crate risiko;
+use risiko::*;
+
+struct Human {
+}
+
+impl Human {
+	fn new() -> Human {
+		Human {}
+	}
+}
+
+impl Player for Human {
+	fn action<'a>(&self, map: &'a Map<'a, Self>) -> Action<'a, Self> {
+		Attack(map.random().unwrap(), map.random().unwrap())
+	}
+}
+
+fn main() {
+	let players: Vec<_> = (0..4).map(|_| Human::new())
+						.collect();
+	let game = Game::new(&players);
+}
